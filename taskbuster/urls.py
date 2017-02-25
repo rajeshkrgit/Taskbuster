53783fb24d7e50f1f8e106ba5809e9de8d000740
Rajesh Kumar <rajeshk12591@gmail.com>
2017-02-25 19:20:29 +0530

    Template Inheritance, Website files and Testing with coverage

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from .views import home
+from .views import home,home_files
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'taskbuster.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', home, name='home'),
+    url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
+        home_files, name='home-files'),
     url(r'^admin/', include(admin.site.urls)),
 
 ]
